6446ec798ad6ada08744129e926c36236de60f66
mathslinux <riegamaths@gmail.com>
2011-06-14 23:18:20 +0800

    first commit

---
+++ gx-lisps/gx-pulse.el
@@ -0,0 +1,49 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; pulse config file ;;
+;; Time-stamp: <2011-06-11-16:35:42 星期六 by geniux>
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'pulse)
+ 
+(defadvice exchange-point-and-mark-nomark (after pulse-advice activate)
+  "Cause the line that is `goto'd to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p)
+		 (> (abs (- (point) (mark))) 400))
+	(pulse-momentary-highlight-one-line (point))))
+ 
+(defadvice switch-to-buffer (after pulse-advice activate)
+  "Cause the current line of new buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+ 
+(defadvice ido-switch-buffer (after pulse-advice activate)
+  "Cause the current line of new buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+ 
+(defadvice switch-to-other-buffer (after pulse-advice activate)
+  "Cause the current line of new buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+ 
+(defadvice visit-.emacs (after pulse-advice activate)
+  "Cause the current line of .emacs buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+ 
+(defadvice beginning-of-buffer (after pulse-advice activate)
+  "Cause the current line of buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+
+(defadvice next-line (after pulse-advice activate)
+  "Cause the current line of buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+
+(defadvice previous-line (after pulse-advice activate)
+  "Cause the current line of buffer to pulse when the cursor gets there."
+  (when (and pulse-command-advice-flag (interactive-p))
+	(pulse-momentary-highlight-one-line (point))))
+
+(pulse-toggle-integration-advice (if window-system 1 -1))
---
+++ gx-lisps/gx-yasnippet.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; yasnippet map config file ;;
+;; Time-stamp: <2011-06-04-00:01:59 星期六 by geniux>
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'load-path (concat lisps-path "/yasnippet-0.6.1c"))
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory (concat lisps-path "/yasnippet-0.6.1c"))
+
+
