177adf74676aea570935b411851ba6dcb56b2efc
mathslinux <riegamaths@gmail.com>
2011-08-23 00:02:05 +0800

    Bugfixed: Emacs cant use ibus method. Feature: Add cscope support

--- my-lisp/S114my-auto-complete.el
+++ my-lisp/S114my-auto-complete.el
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Auto-complete config file ;;
-;; Time-stamp: <2011-06-11-23:34:54 星期六 by geniux>
+;; Time-stamp: <2011-08-22-22:12:22 星期一 by geniux>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defconst auto-complete-path (concat site-lisp-path "auto-complete"))
@@ -9,4 +9,4 @@
 (add-to-list 'ac-dictionary-directories (concat auto-complete-path "lisps/auto-complete/ac-dict"))
 (ac-config-default)
 
-(setq ac-modes (append ac-modes '(org-mode jabber-chat-mode erc-mode html-mode)))
+(setq ac-modes (append ac-modes '(org-mode jabber-chat-mode erc-mode html-mode makefile-mode makefile-gmake-mode)))
