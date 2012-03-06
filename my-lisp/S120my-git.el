9bcd0264f170e450a62902c0b0188c17443a2fbf
mathslinux <riegamaths@gmail.com>
2012-03-06 18:14:04 +0800

    Add git-emacs mode

---
+++ my-lisp/S120my-git.el
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Git config file
+;; Time-stamp: <2012-03-06-18:09:52 星期二 by geniux>
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'load-path (concat site-lisp-path "git-emacs/"))
+(require 'git-emacs)
+(require 'git-emacs-autoloads)
