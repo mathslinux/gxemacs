4059641360d752c2289bfd1466a18f5ca04495c0
mathslinux <riegamaths@gmail.com>
2012-03-13 13:49:24 +0800

    auto-complete: Add semantic backend for C/C++ mode.
    Now use semantic backend to do class member completion.

--- my-lisp/S114my-auto-complete.el
+++ my-lisp/S114my-auto-complete.el
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Auto-complete config file ;;
-;; Time-stamp: <2011-10-27-10:58:14 星期四 by geniux>
+;; Time-stamp: <2012-03-13-13:46:30 星期二 by geniux>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defconst auto-complete-path (concat site-lisp-path "auto-complete"))
@@ -8,6 +8,21 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories (concat auto-complete-path "lisps/auto-complete/ac-dict"))
 (ac-config-default)
+
+;; For semantic backend.
+(add-hook 'c-mode-common-hook
+		  (lambda ()
+			(local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)))
+
+(defun ac-complete-semantic-self-insert (arg)
+    (interactive "p")
+    (self-insert-command arg)
+    (ac-complete-semantic))
+
+(add-hook 'c-mode-common-hook
+		  (lambda ()
+			(local-set-key "." 'ac-complete-semantic-self-insert)
+			(local-set-key ">" 'ac-complete-semantic-self-insert)))
 
 (setq ac-modes (append ac-modes '(org-mode jabber-chat-mode erc-mode
 										   html-mode makefile-gmake-mode
