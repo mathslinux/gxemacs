41a3a140bf54f195c3335b951c6062153279058a
mathslinux <riegamaths@gmail.com>
2012-03-13 17:03:48 +0800

    Cedet: rework cedet config
    Now my cedet is more powerful for C/C++ coding.

--- my-lisp/S105my-cedet.el
+++ my-lisp/S105my-cedet.el
@@ -1,37 +1,96 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; cedet mode config file ;;
-;; Time-stamp: <2012-02-20-13:59:44 星期一 by geniux>
+;; cedet mode config file
+;; See Config_CEDET.org for detail
+;; Time-stamp: <2012-03-13-17:01:11 星期二 by geniux>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
-                                  global-semanticdb-minor-mode
-                                  global-semantic-idle-summary-mode
-                                  global-semantic-mru-bookmark-mode
-								  global-semantic-decoration-mode
-								  global-semantic-highlight-func-mode ))
-(semantic-mode t)
+(load-file (concat site-lisp-path "cedet/common/cedet.el"))
+(require 'semantic-ia)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Semantic configure
+
+;; For C/C++
+(require 'semantic-gcc)
+(defconst cedet-user-include-dirs
+  (list ".." "../include" "../.." "../../include"))
+;; FIXME
+;; (require ’semantic-c nil ‘noerror)
+(mapc (lambda (dir)
+		(semantic-add-system-include dir 'c++-mode)
+		(semantic-add-system-include dir 'c-mode))
+	  cedet-user-include-dirs)
+
+(semantic-load-enable-minimum-features)
+(semantic-load-enable-code-helpers)
+;;(semantic-load-enable-guady-code-helpers)
+(global-semantic-decoration-mode 1)
+;;(global-semantic-decoration-on-includes)
+;; (global-semantic-idle-completions-mode 1)
+;; (semantic-load-enable-excessive-code-helpers)
+(global-semantic-highlight-func-mode 1)
+(global-semantic-idle-local-symbol-highlight-mode 1)
+;;(global-which-function-mode)
+;; (semantic-load-enable-semantic-debugging-helpers)
+(global-semantic-highlight-edits-mode 1)
+
+;; Code fold
+(global-semantic-tag-folding-mode 1)
+
+;; Semantic configure
+(require 'semanticdb)
+(global-semanticdb-minor-mode 1)
+(setq semanticdb-default-save-directory (concat temporary-path "semanticdb/"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; EDE configure
+
+;; (global-ede-mode 1)
+
+;; Enable EDE for a pre-existing C++ project
+;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
+
+;; * This enables the use of Exuberent ctags if you have it installed.
+;;   If you use C++ templates or boost, you should NOT enable it.
+;; (semantic-load-enable-all-exuberent-ctags-support)
+;;   Or, use one of these two types of support.
+;;   Add support for new languges only via ctags.
+;; (semantic-load-enable-primary-exuberent-ctags-support)
+;;   Add support for using ctags as a backup parser.
+;; (semantic-load-enable-secondary-exuberent-ctags-support)
+
+;; Enable SRecode (Template management) minor-mode.
+;; (global-srecode-minor-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+;; (setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+;;                                   global-semanticdb-minor-mode
+;;                                   global-semantic-idle-summary-mode
+;;                                   global-semantic-mru-bookmark-mode
+;; 								  global-semantic-decoration-mode
+;; 								  global-semantic-highlight-func-mode ))
+;; (semantic-mode t)
 
 
-(require 'semantic/analyze/refs)
+;; (require 'semantic/analyze/refs)
 
-;; 代码跳转, 跳回 from emacser.com
-(defun semantic-ia-fast-jump-back ()
-  (interactive)
-  (if (ring-empty-p (oref semantic-mru-bookmark-ring ring))
-      (error "Semantic Bookmark ring is currently empty"))
-  (let* ((ring (oref semantic-mru-bookmark-ring ring))
-         (alist (semantic-mrub-ring-to-assoc-list ring))
-         (first (cdr (car alist))))
-    (if (semantic-equivalent-tag-p (oref first tag) (semantic-current-tag))
-        (setq first (cdr (car (cdr alist)))))
-    (semantic-mrub-switch-tags first)))
+;; ;; 代码跳转, 跳回 from emacser.com
+;; (defun semantic-ia-fast-jump-back ()
+;;   (interactive)
+;;   (if (ring-empty-p (oref semantic-mru-bookmark-ring ring))
+;;       (error "Semantic Bookmark ring is currently empty"))
+;;   (let* ((ring (oref semantic-mru-bookmark-ring ring))
+;;          (alist (semantic-mrub-ring-to-assoc-list ring))
+;;          (first (cdr (car alist))))
+;;     (if (semantic-equivalent-tag-p (oref first tag) (semantic-current-tag))
+;;         (setq first (cdr (car (cdr alist)))))
+;;     (semantic-mrub-switch-tags first)))
 
-(defun semantic-ia-fast-jump-or-back (&optional back)
-  (interactive "P")
-  (if back
-      (semantic-ia-fast-jump-back)
-    (semantic-ia-fast-jump (point))))
-(define-key semantic-mode-map [f12] 'semantic-ia-fast-jump-or-back)
-(define-key semantic-mode-map [C-f12] 'semantic-ia-fast-jump-or-back)
-(define-key semantic-mode-map [S-f12] 'semantic-ia-fast-jump-back) 
-
+;; (defun semantic-ia-fast-jump-or-back (&optional back)
+;;   (interactive "P")
+;;   (if back
+;;       (semantic-ia-fast-jump-back)
+;;     (semantic-ia-fast-jump (point))))
+;; (define-key semantic-mode-map [f12] 'semantic-ia-fast-jump-or-back)
+;; (define-key semantic-mode-map [C-f12] 'semantic-ia-fast-jump-or-back)
+;; (define-key semantic-mode-map [S-f12] 'semantic-ia-fast-jump-back)
