f3266ff5dd2aab0041487a21dc288f036d4f88ca
mathslinux <riegamaths@gmail.com>
2012-07-11 18:01:20 +0800

    ui: Fix plist-to-alist(void)
    Signed-off-by: mathslinux <riegamaths@gmail.com>

--- pkg/color-theme.el
+++ pkg/color-theme.el
@@ -258,8 +258,9 @@ a plist.  In XEmacs, the alist is deprecated; a plist is used instead."
 	 plist)
 	((not (symbolp (car plist)))
 	 (error "Wrong type argument: plist, %S" plist))
-	(t
-	 (plist-to-alist plist)))); XEmacs only
+	;; (t
+	;;  (plist-to-alist plist))
+	)); XEmacs only
 
 ;; Customization
 
