11b5317e79f8bf7d71c9d46cec2dc23b3486285b
Dunrong Huang <riegamaths@gmail.com>
2013-01-24 10:13:41 +0800

    magit: Add magit support
    magit is an Emacs mode for Git
    
    Signed-off-by: Dunrong Huang <riegamaths@gmail.com>

--- my-lisp/S120my-git.el
+++ my-lisp/S120my-git.el
@@ -1,8 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Git config file
-;; Time-stamp: <2012-03-06-18:09:52 星期二 by geniux>
+;; Time-stamp: <2013-01-23-19:45:22 星期三 by geniux>
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'load-path (concat site-lisp-path "git-emacs/"))
-(require 'git-emacs)
-(require 'git-emacs-autoloads)
+(require 'magit)
